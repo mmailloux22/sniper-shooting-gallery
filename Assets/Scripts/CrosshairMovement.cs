cc349774eb669c918581822357c02710e026b6bd
Michael Mailloux <mmailloux22@gmail.com>
2020-08-09 20:32:42 -0400

    Added initial sprites
    Added civilian, target, law enforcement, added moving crosshair

---
+++ Assets/Scripts/CrosshairMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrosshairMovement : MonoBehaviour
+{
+    private Vector2 mousePosition;
+    public float moveSpeed = 1f;
+
+    void Awake() 
+    {
+        Cursor.visible = false;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
+    }
+}

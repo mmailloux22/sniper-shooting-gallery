52d2f6757925c1f402bbd6a95dae30f3fef23637
Michael Mailloux <mmailloux22@gmail.com>
2020-08-11 21:11:39 -0400

    Added Person methods to target and cop
    Added Person methods to target and cop

---
+++ Assets/Scripts/Cop.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cop : MonoBehaviour, Person
+{
+    public void spawn()
+    {
+
+    }
+
+    public void move()
+    {
+
+    }
+
+    public void die()
+    {
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

e4e1fd2b2bb6b3c9f7a1bf90d82b34692026052b
Michael Mailloux <mmailloux22@gmail.com>
2020-08-17 20:50:14 -0400

    Updated Spawner script
    Added spawn methods,  added person percentages and total population to Spawner script

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -7,22 +7,50 @@ public class Spawner : MonoBehaviour
     public GameObject secretAgent;
     public GameObject civilian;
     public GameObject target;
+    public float population;
+    public float civilianPercent;
+    public float targetPercent;
+    public float secretAgentPercent;
+    private int civilianAmount;
+    private int targetAmount;
+    private int secretAgentAmount;
+
     void Awake() 
     {
-        for (int i = 0; i < 5;i++)
+        population = 40;
+        civilianAmount = (int) (civilianPercent / 100 * population);
+        targetAmount = (int) (targetPercent / 100 * population);
+        secretAgentAmount = (int) (secretAgentPercent / 100 * population);
+
+        spawnSecretAgents();
+        spawnCivilians();
+        spawnTargets();
+
+    }
+
+    void spawnSecretAgents() 
+    {
+        for (int i = 0; i < secretAgentAmount; i++)
         {
             Instantiate(secretAgent, new Vector2(0, 0), Quaternion.identity);
         }
-        for (int i = 0; i < 10; i++)
+    }
+
+    void spawnCivilians()
+    {
+        for (int i = 0; i < civilianAmount; i++)
         {
             Instantiate(civilian, new Vector2(0, 0), Quaternion.identity);
         }
-        for (int i = 0; i < 3; i++)
+    }
+
+    void spawnTargets()
+    {
+        for (int i = 0; i < targetAmount; i++)
         {
             Instantiate(target, new Vector2(0, 0), Quaternion.identity);
         }
     }
-
     // Start is called before the first frame update
     void Start()
     {
